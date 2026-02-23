92529f67f1f972f68917b7a83c4b975402d015bc
Pandas720 <alvaradu6230@student.faytechcc.edu>
2026-02-22 21:57:11 -0500

    Add files via upload

---
+++ P1LAB1_AlvaradoUbaldo.py
@@ -0,0 +1,8 @@
+ # Ubaldo Alvarado
+ # February 22, 2026
+ # P1LAB1
+ # Create a program that allows the user to enter their first name and last name. The user should be prompted to enter the first and last names separately and each name should be assigned to its own variable. The program should then display a message welcoming the user to this course.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello, " + first_name + " " + last_name + "!" + " Welcome to CTI-110")
